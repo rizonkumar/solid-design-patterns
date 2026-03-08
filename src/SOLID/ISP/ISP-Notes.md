d28cc648aaed6542340d4d2e44243fdfdbe2d473
rizonkumar <rizon.kumar.rahi@gmail.com>
2026-03-08 11:09:00 +0530

    Add SOLID principles documentation and examples
    Introduced a comprehensive README for the SOLID principles, detailing each principle with links to guides and examples of both bad and good code implementations. Added specific notes for the Dependency Inversion Principle (DIP) and included bad and good code examples for Single Responsibility Principle (SRP), Open/Closed Principle (OCP), Interface Segregation Principle (ISP), and Liskov Substitution Principle (LSP). This enhances understanding of SOLID principles and promotes better software design practices.

--- src/SOLID/ISP/ISP-Notes.md
+++ src/SOLID/ISP/ISP-Notes.md
@@ -5,8 +5,7 @@ The **Interface Segregation Principle (ISP)** states that:
 > "No client should be forced to depend on methods it does not use."
 
 In simpler terms, a class should **not be burdened with unnecessary methods** it doesn’t need.  
-Instead of creating one large, general-purpose interface, it’s better to create **multiple smaller, specific interfaces
-** tailored to individual needs.
+Instead of creating one large, general-purpose interface, it’s better to create **multiple smaller, specific interfaces** tailored to individual needs.
 
 ### Benefits
 
--- src/SOLID/OCP/BadCode/PaymentProcessor.java
+++ src/SOLID/OCP/BadCode/PaymentProcessor.java
@@ -4,15 +4,15 @@ public class PaymentProcessor {
     public void processPayment(String paymentMethod, double amount) {
         if(paymentMethod.equals("CreditCard")) {
             // business logic
-            System.out.println("Payment via credit card" + amount);
+            System.out.println("Payment via credit card: " + amount);
         }
         else if(paymentMethod.equals("DebitCard")) {
             // business logic
-            System.out.println("Payment via debit card" + amount);
+            System.out.println("Payment via debit card: " + amount);
         }
         else if(paymentMethod.equals("Paypal")) {
             // business logic
-            System.out.println("Payment via paypal" + amount);
+            System.out.println("Payment via paypal: " + amount);
         }
         else {
             throw new IllegalArgumentException("Unsupported payment method" + paymentMethod);
