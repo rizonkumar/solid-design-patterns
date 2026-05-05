f2931533e3af4946c3e7b79acaafacbfcaddbddc
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2026-05-05 10:20:05 +0530

    Feat: Added Product Class

--- src/OOOD/04-vending-machine/src/vending/Product.java
+++ src/OOOD/04-vending-machine/src/vending/Product.java
@@ -1,1 +1,15 @@
 package vending;
+
+import java.math.BigDecimal;
+
+class Product {
+    final String productCode;
+    final String description;
+    final BigDecimal unitPrice;
+
+    public Product(String productCode, String description, BigDecimal unitPrice) {
+        this.productCode = productCode;
+        this.description = description;
+        this.unitPrice = unitPrice;
+    }
+}
